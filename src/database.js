322d5c5c62397dd486dd5012684db9cf0023f576
shekhnazarov <umarshexnazarov@gmail.com>
2023-07-07 01:01:15 +0500

    React Components Homework

---
+++ src/database.js
@@ -0,0 +1,14 @@
+export const database = [
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/slices-of-watermelon.jpg?w=600&quality=80", title: "Watermelon"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/slices-of-orange.jpg?w=600&quality=80", title: "Orange"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/cherries-pattern-free-photo.jpg?w=600&quality=80", title: "Cherry"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/pile-of-gala-and-granny-smith-apples-on-market.jpg?w=600&quality=80", title: "Apple"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/pile-of-blueberries.jpg?w=600&quality=80", title: "Blueberry"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/bananas-with-red-flat-background.jpg?w=600&quality=80", title: "Banana"},
+    {src: "https://image.shutterstock.com/image-photo/coconut-juice-half-fruit-isolated-260nw-2155552059.jpg", title: "Coconut"},
+    {src: "https://image.shutterstock.com/image-photo/dried-papaya-fresh-leaves-260nw-2321731603.jpg", title: "Papaya"},
+    {src: "https://image.shutterstock.com/image-photo/fresh-white-pink-red-purple-260nw-2182160531.jpg", title: "Dragon Fruit"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/fresh-dark-blue-grape-free-photo.jpg?w=600&quality=80", title: "Grape"},
+    {src: "https://i0.wp.com/picjumbo.com/wp-content/uploads/homemade-mango-popsicles-decorated-with-mint-leaves.jpg?w=600&quality=80", title: "Mango"},
+    {src: "https://image.shutterstock.com/image-photo/durian-fruit-slices-isolated-on-260nw-2149051435.jpg", title: "Durian"},
+]
